themaven-net/draft-js-import-element

commit d5c35d5be355f806b679ae9736ebbf0392196574

fixes bug where IE 11 will have a null element (#14)

src/stateFromElement.js
@@ -237,6 +237,7 @@ class BlockGenerator {
   }
 
   processBlockElement(element: DOMElement) {
+    if(!element) return
     let tagName = element.nodeName.toLowerCase();
     let type = this.getBlockTypeFromTagName(tagName);
     let hasDepth = canHaveDepth(type);

commit 4336b6d47306e721648b22ad225b460ee5795732

Fix eslint issue introduced in #14

src/stateFromElement.js
@@ -237,7 +237,9 @@ class BlockGenerator {
   }
 
   processBlockElement(element: DOMElement) {
-    if(!element) return
+    if (!element) {
+      return;
+    }
     let tagName = element.nodeName.toLowerCase();
     let type = this.getBlockTypeFromTagName(tagName);
     let hasDepth = canHaveDepth(type);